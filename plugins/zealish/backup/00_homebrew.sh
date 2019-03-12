8e574dcafb5181569c37d8a8de1772fc06c11b2e
zealic <zealic@gmail.com>
2019-03-12 21:36:50 +0800

    * Backup home brew with hostname.

--- plugins/zealish/backup/00_homebrew.sh
+++ plugins/zealish/backup/00_homebrew.sh
@@ -1,2 +1,8 @@
-brew leaves > ~/Dropbox/Backup/Homebrew.lst
-brew cask list > ~/Dropbox/Backup/Homebrew-Caskroom.lst
+BACKUP_DIR=$HOME/Dropbox/Workspace/$(hostname)
+
+if [[ ! -d "$BACKUP_DIR" ]]; then
+  mkdir -p "$BACKUP_DIR"
+fi
+
+brew leaves > $BACKUP_DIR/Homebrew.lst
+brew cask list > $BACKUP_DIR/Homebrew-Caskroom.lst
