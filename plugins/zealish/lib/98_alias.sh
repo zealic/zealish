64512a892283395ea3e14427640aecc765fda3e9
zealic <zealic@gmail.com>
2020-09-04 12:36:08 +0800

    feat: Improve chrome/unsafe-chrome with tenant.

--- plugins/zealish/lib/98_alias.sh
+++ plugins/zealish/lib/98_alias.sh
@@ -1,7 +1,22 @@
 # aliases
 alias docker4mac-shell="docker run -it --privileged --pid=host debian nsenter -t 1 -m -u -n -i sh"
 
-if [[ ! -d $HOME/.unsafe-chrome ]]; then
-  mkdir -p $HOME/.unsafe-chrome
-fi
-alias unsafe-chrome="open -n -a \"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome\" --args --user-data-dir=\"$HOME/.unsafe-chrome\" --disable-web-security"
+chrome() {
+  local chrome_opts=${CHROME_OPTS}
+  local chromeBin="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+  local tenant="${CHROME_TENANT:-$1}"
+  local tenantDir
+  if [[ -z "$tenant" ]]; then
+    echo "Tenant name is required."
+    return 1
+  fi
+  local tenantDir="$HOME/.zchrome/$tenant"
+  if [[ ! -d "$tenantDir" ]]; then
+    mkdir -p "$tenantDir" || return 1
+  fi
+  open -n -a "$chromeBin" --args --user-data-dir="$tenantDir" $chrome_opts
+}
+
+unsafe-chrome() {
+  CHROME_OPTS=--disable-web-security chrome $@
+}
