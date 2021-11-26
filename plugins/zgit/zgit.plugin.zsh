63554b43050a5efd0b58a66191ead79353f649ae
zealic <zealic@gmail.com>
2021-11-26 11:07:57 +0800

    improve: Config git pull fast forward only.

--- plugins/zgit/zgit.plugin.zsh
+++ plugins/zgit/zgit.plugin.zsh
@@ -4,5 +4,6 @@ if [ ! -d $GITCONFIG_USERS_DIR ]; then
   mkdir -p $GITCONFIG_USERS_DIR 2>/dev/null
 fi
 git config --global user.useConfigOnly true
+git config --global pull.ff only
 git config --global alias.set-user '!bash -c "git config include.path ~/.gitconfig.d/users/$1.conf" -'
 
